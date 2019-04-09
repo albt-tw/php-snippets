8153ca69eee2f2e5f37c706dc7971ad1d5d148bd
albt-tw <35334185+albt-tw@users.noreply.github.com>
2019-04-09 17:50:38 +0800

    Create base64url.php

---
+++ Support/Helpers/base64url.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('base64url_encode')) {
+    function base64url_encode($data)
+    {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
+}
+
+if (!function_exists('base64url_decode')) {
+    function base64url_decode($data)
+    {
+        return base64_decode(strtr($data, '-_', '+/') . substr('===', (strlen($data) + 3) % 4));
+    }
+}
